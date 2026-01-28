94816116150ca6088ac7f2c5cef28c0c352953fc
Jeremiah Hughes <hughjere@shernet.sheridancollege.ca>
2026-01-28 11:03:07 -0500

    Added a button to the UI that changed color and size and also hide the game object

---
+++ Assets/Scripts/UIDemo.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class UIDemo : MonoBehaviour
+{
+    public SpriteRenderer sr;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.anyKey.wasPressedThisFrame == true)
+        {
+            ChangeColour();
+        }
+    }
+    public void ChangeColour()
+        {
+        sr.color = Random.ColorHSV();
+        }
+
+    public void SetSize(float size)
+    {
+        transform.localScale = Vector3.one * size;
+    }
+}
